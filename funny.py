709aebda3c351b9ff08c9fcd750bd9d9f937a315
StandyFy <106475223+StandyFy@users.noreply.github.com>
2025-01-06 19:25:41 +0100

    WindowGoWRoom
    Makes window go Wroom.
    Just a funny python script.

---
+++ funny.py
@@ -0,0 +1,8 @@
+import pygetwindow as oeriujgsedoighjrjghrdsoigjhsreiok; import pyautogui; from time import sleep as sdoilrgjosrdihjgseiogseoipgjwsepiogjseiojgsigjsejikogji; 
+while 1: 
+    isuehfiwshjhfbnewshijfhsehf = oeriujgsedoighjrjghrdsoigjhsreiok.getActiveWindow()
+    if isuehfiwshjhfbnewshijfhsehf:
+            oWSIUEHFIKEJSHFWSEIUHFWSIOEFHJSEOIFHSIOEFHJSEOI, iukhfsiujhfseijfhesfheufhseikjfhsejikfhesujikfhesjifhesj = isuehfiwshjhfbnewshijfhsehf.left, isuehfiwshjhfbnewshijfhsehf.top
+            isuehfiwshjhfbnewshijfhsehf.restore()
+    isuehfiwshjhfbnewshijfhsehf.moveTo(oWSIUEHFIKEJSHFWSEIUHFWSIOEFHJSEOIFHSIOEFHJSEOI-10, iukhfsiujhfseijfhesfheufhseikjfhsejikfhesujikfhesjifhesj)
+    sdoilrgjosrdihjgseiogseoipgjwsepiogjseiojgsigjsejikogji(.1)
